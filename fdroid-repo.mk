5d709ae8c8b82d0afcdfae176b789d1a4cd7dc5b
Michael Bestas <mkbestas@gmail.com>
2025-03-06 16:59:13 +0200

    DNM HACK to make build start
    Change-Id: Ibe370ecc103040b1f2a45714fc20a0b26524d1c9

--- fdroid-repo.mk
+++ fdroid-repo.mk
@@ -1,50 +1,5 @@
 # Auto generated, do not edit.
 
-PRODUCT_COPY_FILES += \
-    prebuilts/calyx/fdroid/fallback-icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/fallback-icon.png \
-    prebuilts/calyx/fdroid/repo/app.organicmaps/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/app.organicmaps/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/at.bitfire.davdroid/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/at.bitfire.davdroid/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/com.aurora.store/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.aurora.store/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/com.fsck.k9/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.fsck.k9/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/com.jarsilio.android.scrambledeggsif/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.jarsilio.android.scrambledeggsif/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/com.nextcloud.client/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.nextcloud.client/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/diff/1739332897000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739332897000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739419303000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739419303000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739505693000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739505693000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739592088000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739592088000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739678501000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739678501000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739764891000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739764891000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739851297000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739851297000.json \
-    prebuilts/calyx/fdroid/repo/diff/1739937689000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1739937689000.json \
-    prebuilts/calyx/fdroid/repo/diff/1740024087000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1740024087000.json \
-    prebuilts/calyx/fdroid/repo/diff/1740062470000.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/diff/1740062470000.json \
-    prebuilts/calyx/fdroid/repo/entry.jar:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/entry.jar \
-    prebuilts/calyx/fdroid/repo/entry.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/entry.json \
-    prebuilts/calyx/fdroid/repo/icons/fdroid-icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons/fdroid-icon.png \
-    prebuilts/calyx/fdroid/repo/index.css:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.css \
-    prebuilts/calyx/fdroid/repo/index.html:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.html \
-    prebuilts/calyx/fdroid/repo/index.jar:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.jar \
-    prebuilts/calyx/fdroid/repo/index.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.png \
-    prebuilts/calyx/fdroid/repo/index-v1.jar:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index-v1.jar \
-    prebuilts/calyx/fdroid/repo/index-v1.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index-v1.json \
-    prebuilts/calyx/fdroid/repo/index-v2.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index-v2.json \
-    prebuilts/calyx/fdroid/repo/index.xml:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.xml \
-    prebuilts/calyx/fdroid/repo/org.breezyweather/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.breezyweather/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.breezyweather/fr/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.breezyweather/fr/icon.png \
-    prebuilts/calyx/fdroid/repo/org.breezyweather/it/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.breezyweather/it/icon.png \
-    prebuilts/calyx/fdroid/repo/org.briarproject.briar.android/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.briarproject.briar.android/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.calyxinstitute.vpn/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.calyxinstitute.vpn/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.fdroid.basic/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.fdroid.basic/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.fdroid.fdroid/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.fdroid.fdroid/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.onionshare.android.fdroid/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.onionshare.android.fdroid/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.openobservatory.ooniprobe/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.openobservatory.ooniprobe/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.thoughtcrime.securesms/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.thoughtcrime.securesms/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.torproject.android/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.torproject.android/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.torproject.torbrowser/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.torproject.torbrowser/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/se.leap.riseupvpn/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/se.leap.riseupvpn/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/status/running.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/status/running.json \
-    prebuilts/calyx/fdroid/repo/status/update.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/status/update.json \
-
 PRODUCT_PACKAGES += \
     fdroid-repo \
     fdroid-debug-repo \
